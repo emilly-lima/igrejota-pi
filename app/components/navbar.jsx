ab7c3eb5228df4e4b1ca4200d9622ca18b5e86b5
emilly.lima <emilly.blima25@gmail.com>
2023-11-06 03:01:26 -0300

    add: ajuste position da navbar

--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -1,8 +1,19 @@
+import * as React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faMagnifyingGlass, faQuestion } from '@fortawesome/free-solid-svg-icons'
+
+let offsetHeight;
 export default function Navbar() {
+    const headerRef = React.useRef(null);
+    const [height, setHeight] = React.useState(0);
+
+    React.useLayoutEffect(() => {
+        setHeight(headerRef.current.offsetHeight);
+        offsetHeight = height;
+    }, [headerRef.current])
+
     return (
-        <div className="w-full flex px-16 bg-zinc-800 h-20 items-center place-content-between gap-2 md:gap-8">
+        <div ref={headerRef} className="w-full flex px-16 fixed z-[1000] bg-zinc-800 h-20 items-center place-content-between gap-2 md:gap-8">
             <h1 className="text-white font-bold">IGREJOTA</h1>
             <div className='flex flex-row items-center'>
                 <div className='flex flex-row relative mx-9'>
