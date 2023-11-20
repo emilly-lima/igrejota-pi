fae51b01c8ea2f280c682286898217069cd0921a
Marilia Noroes <mnoroes2004@gmail.com>
2023-11-20 01:27:13 -0300

    navbar

--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -18,19 +18,19 @@ export default function Navbar() {
     const [openModal, setOpenModal] = useState(false)
 
     return (
-        <div /*ref={headerRef}*/ className="w-full flex px-16 z-[1000] bg-zinc-800 h-20 items-center place-content-between gap-2 md:gap-8">
-            <h1 className="text-white font-bold">IGREJOTA</h1>
-            <div className='flex flex-row items-center'>
+        <div /*ref={headerRef}*/ className="w-full flex px-16 z-[1000] bg-zinc-800 h-[108px] items-center place-content-between gap-2 md:gap-8 ">
+            <h1 className="text-white font-bold ml-[130px]">IGREJOTA</h1>
+            <div className='flex flex-row items-center mr-[130px]'>
                 <div className='flex flex-row relative mx-9'>
-                    <input type="text" placeholder="Buscar jogo.." />
-                    <FontAwesomeIcon icon={faMagnifyingGlass} width={24} height={24} className='absolute top-2 right-2 pointer-events-none' />
+                    <input type="text" placeholder="Buscar jogo..." className='bg-zinc-200  rounded-[16px] w-[400px] h-[43px] mr-[30px] hover:bg-white'/>
+                    <FontAwesomeIcon icon={faMagnifyingGlass} width={24} height={24} className='absolute top-2.5 left-[365px] w-[22px] h-[22px] pointer-events-none ' />
                 </div>
-                <button className='px-4' onClick={() => setOpenModal(true)}>
-                    <FontAwesomeIcon icon={faQuestion} width={30} height={30} />
+                <button className='px-4 rounded-[16px] w-[50px] h-[54px] bg-zinc-200 hover:bg-white' onClick={() => setOpenModal(true)}>
+                    <FontAwesomeIcon icon={faQuestion} width={30} height={30} className='flex items-center w-[20px] h-[37px]'/>
                 </button>
                 <div>
-                    <Modal isOpen={openModal} setModalOpen={() =>setOpenModal(!openModal)}>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vulputate mi tristique hendrerit rhoncus. Quisque maximus enim eget mauris imperdiet tincidunt. Pellentesque commodo consequat diam vitae tincidunt. Phasellus mollis sem lobortis volutpat accumsan. Nam auctor nulla porttitor venenatis dictum. Proin venenatis turpis sit amet ante malesuada, id tempor metus consectetur. Sed vulputate accumsan lacus vitae egestas. Quisque ut elit in ligula lobortis semper. Quisque vehicula massa euismod risus posuere, sit amet accumsan elit mattis. Pellentesque est enim, feugiat vitae ullamcorper nec, ullamcorper vitae eros. Ut eu hendrerit dui.
+                    <Modal isOpen={openModal} setModalOpen={() => setOpenModal(!openModal)}>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vulputate mi tristique hendrerit rhoncus. Quisque maximus enim eget mauris imperdiet tincidunt. Pellentesque commodo consequat diam vitae tincidunt. Phasellus mollis sem lobortis volutpat accumsan. Nam auctor nulla porttitor venenatis dictum. Proin venenatis turpis sit amet ante malesuada, id tempor metus consectetur. Sed vulputate accumsan lacus vitae egestas. Quisque ut elit in ligula lobortis semper. Quisque vehicula massa euismod risus posuere, sit amet accumsan elit mattis. Pellentesque est enim, feugiat vitae ullamcorper nec, ullamcorper vitae eros. Ut eu hendrerit dui.
                     </Modal>
                 </div>
             </div>
