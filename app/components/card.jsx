562e9ffc46cbb5d95f500b06d3e2ff1108d3e442
Emilly Lima <97124187+emilly-lima@users.noreply.github.com>
2023-11-20 01:27:54 -0300

    css dos cards e img

--- app/components/card.jsx
+++ app/components/card.jsx
@@ -4,11 +4,16 @@ const Card = ({ jogos }) => {
     return (
         <div className='flex flex-row gap-[58px] justify-center py-6 flex-wrap relative bottom-0'>
             {jogos && jogos.map((item) => (
-                <div key={item.Jogo} className="w-[200px] h-[300px] bg-zinc-500">
-                    <h2>{item.Jogo}</h2>
-                    <p>{item.Tempo}</p>
-                    <p>{item.Jogadores}</p>
-                    <p>{item.Formato}</p>
+                <div className='relative'>
+                    <div key={item.Jogo} className="md:w-[200px] md:h-[300px] bg-zinc-400 cursor-pointer items-center flex flex-col gap-2 p-5">
+                        <div className='absolute bg-zinc-500 top-0 left-0 h-full w-full rotate-6 z-[-1] text-center' />
+                        <img src={item.Img} alt={item.Jogo} />
+                        <h3>{item.Jogo}</h3>
+                        <p>{item.Tempo}</p>
+                        <p>{item.Jogadores}</p>
+                        <p>{item.Formato}</p>
+                        
+                    </div>
                 </div>
             ))}
         </div>
