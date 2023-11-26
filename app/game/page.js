c454169dc2f3ccd008a201fae5a1cc8adca88e64
Emilly Lima <97124187+emilly-lima@users.noreply.github.com>
2023-11-26 17:52:27 -0300

    carousel atualizado

--- app/game/page.js
+++ app/game/page.js
@@ -1,17 +1,38 @@
 "use client";
 import React from "react";
 import Navbar from "@/app/components/navbar";
-import CustomCarousel from "@/app/components/carrossel";
+import Ogame from "@/app/components/game";
 
 export default function Game() {
+    const [jogos, setJogos] = React.useState([]);
+
+    React.useEffect(() => {
+        const fetchGames = async () => {
+            try {
+                const response = await fetch("jogos.json");
+                const games = await response.json();
+                console.log(games);
+                setJogos(games);
+            } catch (error) {
+                console.error("Erro ao buscar os jogos:", error);
+            }
+        };
+
+        fetchGames();
+    }, []);
+
     return (
-        <div>
+        <div className="flex flex-col h-screen">
             <header>
                 <Navbar />
             </header>
-            <main>
-                
-            </main>
+            <div className="flex flex-row h-full">
+                <div className="w-1/5"></div>
+                <main className="flex w-3/5 bg-zinc-600">
+                    <Ogame jogos={jogos} />
+                </main>
+                <div className="w-1/5"></div>
+            </div>
         </div>
     )
 }
--- app/second/page.js
+++ app/second/page.js
@@ -3,71 +3,79 @@ import React from "react";
 import Navbar from "@/app/components/navbar";
 import Random from "../components/random";
 import Card from "../components/card";
+import { useGameTime } from "../store/gameTime";
 
 export default function Second() {
-    const [jogos, setJogos] = React.useState([]);
+  const [jogos, setJogos] = React.useState([]);
+  const { setTime, time } = useGameTime();
 
-    React.useEffect(() => {
-        const fetchGames = async () => {
-            try {
-                const response = await fetch("jogos.json");
-                const games = await response.json();
-                console.log(games);
-                setJogos(games);
-            } catch (error) {
-                console.error("Erro ao buscar os jogos:", error);
-            }
-        };
+  React.useEffect(() => {
+    const fetchGames = async () => {
+      try {
+        const response = await fetch("jogos.json");
+        const games = await response.json();
+        console.log(games);
+        setJogos(games);
+      } catch (error) {
+        console.error("Erro ao buscar os jogos:", error);
+      }
+    };
 
-        fetchGames();
-    }, []);
-    // rest of your component
-    return (
-        <div className="m-auto max-w-[100vw]">
-            <Navbar />
-            <section className="flex justify-evenly bg-zinc-300 w-full p-4">
-                <select className="appearence-none" id="tempo">
-                    <option value="todos-p">Tempo de Partida</option>
-                    <option value="30">Menor que 30min</option>
-                    <option value="60">Entre 30 e 60min</option>
-                    <option value="90">Maior que 60min</option>
-                </select>
-                <select id="jogadores">
-                    <option value="todos-j">Nº Jogadores</option>
-                    <option value="1">Um</option>
-                    <option value="2">Dois</option>
-                    <option value="3">Mais de 3</option>
-                </select>
-                <select id="formato">
-                    <option value="todos-f">Formato do Jogo</option>
-                    <option value="tabuleiro">Tabuleiro</option>
-                    <option value="carta">Cartas</option>
-                    <option value="Dados">Dados</option>
-                </select>
-                <select>
-                    <option value="todos-e">Estilo de Jogo</option>
-                    <option value="competitivo">Competitivo</option>
-                    <option value="cooperativo">Cooperativo</option>
-                    <option value="ambos">Ambos</option>
-                </select>
-                <select id="complexidade">
-                    <option value="todos-c">Complexidade</option>
-                    <option value="facil">Fácil</option>
-                    <option value="medio">Médio</option>
-                    <option value="dificil">Dificil</option>
-                </select>
+    fetchGames();
+  }, []);
 
-                <Random />
-            </section>
+  console.log(time);
 
-            <aside>
+  return (
+    <div className="m-auto max-w-[100vw]">
+      <Navbar />
+      <section className="flex w-full p-4 justify-evenly bg-zinc-300">
+        <select
+          className="appearence-none"
+          id="tempo"
+          onClick={(e) => {
+            setTime(e.target.value);
+          }}
+          defaultValue={time}
+        >
+          <option value={0}>Tempo de Partida</option>
+          <option value={1}>Menor que 30min</option>
+          <option value={2}>Entre 30 e 60min</option>
+          <option value={3}>Maior que 60min</option>
+        </select>
+        <select id="jogadores">
+          <option value="todos-j">Nº Jogadores</option>
+          <option value="1">Um</option>
+          <option value="2">Dois</option>
+          <option value="3">Mais de 3</option>
+        </select>
+        <select id="formato">
+          <option value="todos-f">Formato do Jogo</option>
+          <option value="tabuleiro">Tabuleiro</option>
+          <option value="carta">Cartas</option>
+          <option value="Dados">Dados</option>
+        </select>
+        <select>
+          <option value="todos-e">Estilo de Jogo</option>
+          <option value="competitivo">Competitivo</option>
+          <option value="cooperativo">Cooperativo</option>
+          <option value="ambos">Ambos</option>
+        </select>
+        <select id="complexidade">
+          <option value="todos-c">Complexidade</option>
+          <option value="facil">Fácil</option>
+          <option value="medio">Médio</option>
+          <option value="dificil">Dificil</option>
+        </select>
 
-            </aside>
+        <Random />
+      </section>
 
-            <main className="w-full h-screen">
-                <Card jogos={jogos} />
-            </main>
+      <aside></aside>
 
-        </div>
-    )
+      <main className="w-full h-screen">
+        <Card jogos={jogos} />
+      </main>
+    </div>
+  );
 }
