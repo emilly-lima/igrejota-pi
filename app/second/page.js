0f010a6f83b8910dd616352637d0bb0502394d57
Emilly Lima <97124187+emilly-lima@users.noreply.github.com>
2023-11-26 20:26:48 -0300

    commit revert

--- app/second/page.js
+++ app/second/page.js
@@ -19,57 +19,6 @@ export default function Second() {
       }
     };
 
-<<<<<<< HEAD
-    fetchGames();
-  }, []);
-
-  console.log(time);
-
-  return (
-    <div className="m-auto max-w-[100vw]">
-      <Navbar />
-      <section className="flex w-full p-4 justify-evenly bg-zinc-300">
-        <select
-          className="appearence-none"
-          id="tempo"
-          onClick={(e) => {
-            setTime(e.target.value);
-          }}
-          defaultValue={time}
-        >
-          <option value={0}>Tempo de Partida</option>
-          <option value={1}>Menor que 30min</option>
-          <option value={2}>Entre 30 e 60min</option>
-          <option value={3}>Maior que 60min</option>
-        </select>
-        <select id="jogadores">
-          <option value="todos-j">Nº Jogadores</option>
-          <option value="1">Um</option>
-          <option value="2">Dois</option>
-          <option value="3">Mais de 3</option>
-        </select>
-        <select id="formato">
-          <option value="todos-f">Formato do Jogo</option>
-          <option value="tabuleiro">Tabuleiro</option>
-          <option value="carta">Cartas</option>
-          <option value="Dados">Dados</option>
-        </select>
-        <select>
-          <option value="todos-e">Estilo de Jogo</option>
-          <option value="competitivo">Competitivo</option>
-          <option value="cooperativo">Cooperativo</option>
-          <option value="ambos">Ambos</option>
-        </select>
-        <select id="complexidade">
-          <option value="todos-c">Complexidade</option>
-          <option value="facil">Fácil</option>
-          <option value="medio">Médio</option>
-          <option value="dificil">Dificil</option>
-        </select>
-
-        <Random />
-      </section>
-=======
         fetchGames();
     }, []);
     // rest of your component
@@ -117,19 +66,10 @@ export default function Second() {
             <Random />
             <aside>
             </aside>
->>>>>>> 2bbf10309a76e8fd901f0639c6fc5f68c3acf719
 
       <aside></aside>
 
-<<<<<<< HEAD
-      <main className="w-full h-screen">
-        <Card jogos={jogos} />
-      </main>
-    </div>
-  );
-=======
         </div>
         </div>
     )
->>>>>>> 2bbf10309a76e8fd901f0639c6fc5f68c3acf719
 }
