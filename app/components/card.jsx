8c2b557d222ca67dc2bd65fbe3b555c67c5d7a0b
Emilly Lima <97124187+emilly-lima@users.noreply.github.com>
2023-11-29 23:20:47 -0300

    css card

--- app/components/card.jsx
+++ app/components/card.jsx
@@ -6,9 +6,9 @@ const Card = ({ jogos }) => {
             {jogos && jogos.map((item) => (
                 <Link href="/game" key={item.Jogo}>
                     <div className='relative '>
-                        <div key={item.Jogo} id='carta-super' className="w-[145px] h-[220px] md:w-[200px] md:h-[300px] cursor-pointer items-center flex flex-col gap-2 p-5 aspect-square object-fill ">
-                            <div className='absolute bg-zinc-500 top-0 left-0 h-full w-full rotate-6 z-[-1] text-center' />
-                            <img src={item.Img} alt={item.Jogo} />
+                        <div key={item.Jogo} id='carta-super' className="w-[145px] h-[220px] md:w-[200px] md:h-[300px] cursor-pointer items-center flex flex-col gap-2 p-5 rounded-xl">
+                            <div id='carta-infer' className='absolute top-0 left-0 h-full w-full rotate-6 z-[-1] text-center rounded-xl' />
+                            <img className='aspect-square object-fill rounded-xl' src={item.Img} alt={item.Jogo} />
                             <h3>{item.Jogo}</h3>
                             <p>{item.Tempo}</p>
                             <p>{item.Jogadores}</p>
