aa8c6117983429b5e8202434ba5d62ab4e344d71
Emilly Lima <97124187+emilly-lima@users.noreply.github.com>
2023-11-29 23:26:33 -0300

    css no globals

--- app/components/card.jsx
+++ app/components/card.jsx
@@ -6,12 +6,14 @@ const Card = ({ jogos }) => {
             {jogos && jogos.map((item) => (
                 <Link href="/game" key={item.Jogo}>
                     <div className='relative '>
-                        <div key={item.Jogo} id='carta-super' className="w-[145px] h-[220px] md:w-[200px] md:h-[300px] cursor-pointer items-center flex flex-col gap-2 p-5 rounded-xl">
-                            <div id='carta-infer' className='absolute top-0 left-0 h-full w-full rotate-6 z-[-1] text-center rounded-xl' />
+                        <div key={item.Jogo} id='carta-super'>
+                            <div id='carta-infer' />
                             <img className='aspect-square object-fill rounded-xl' src={item.Img} alt={item.Jogo} />
                             <h3>{item.Jogo}</h3>
                             <p>{item.Tempo}</p>
-                            <p>{item.Jogadores}</p>
+                            <p>{item.Njoga}</p>
+                            <p>{item.Peso}</p>
+                            <p>{item.Estilo}</p>
                             <p>{item.Formato}</p>
                         </div>
                     </div>
